1bc74771bce2a9ec85103ff7b4bac16c8b814903
M.P. Korstanje <rien.korstanje@gmail.com>
2022-10-10 14:22:20 +0200

    Update Spring Boot to v2.7.4

---
+++ dynamic-property-source/src/test/java/com/logarithmicwhale/demo/DemoApplication2Test.java
@@ -0,0 +1,38 @@
+package com.logarithmicwhale.demo;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
+
+@Slf4j
+@SpringBootTest
+@Testcontainers
+class DemoApplication2Test {
+
+    static final DockerImageName image = DockerImageName
+            .parse("postgres")
+            .withTag("14.1");
+
+    @Container
+    static final PostgreSQLContainer<?> postgresContainer = new PostgreSQLContainer<>(image)
+            .withReuse(true);
+
+    @DynamicPropertySource
+    static void properties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", postgresContainer::getJdbcUrl);
+        registry.add("spring.datasource.username", postgresContainer::getUsername);
+        registry.add("spring.datasource.password", postgresContainer::getPassword);
+    }
+
+    @Test
+    void contextLoads() {
+        log.info("Hello world");
+    }
+
+}
